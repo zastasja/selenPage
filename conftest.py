053e2fa0328c45c884b5ce31ab3566414f2321f6
zastasja <anastasia_zh@bk.ru>
2022-11-06 18:50:32 +0300

    updated actions file

--- conftest.py
+++ conftest.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.keys import Keys
 
 @pytest.fixture(scope='function')
 def browser():
+    o = webdriver.ChromeOptions()
+    o.headless = True
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield browser
     browser.quit()
