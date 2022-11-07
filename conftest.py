c5b359fa972ec7bc984d6638648da781a2a5adf6
zastasja <anastasia_zh@bk.ru>
2022-11-07 22:25:55 +0300

    added firefox

--- conftest.py
+++ conftest.py
@@ -1,14 +1,23 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 
 
 @pytest.fixture(scope='function')
-def browser():
+
+def init_chrome_browser():
     o = webdriver.ChromeOptions()
     o.headless = True
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield browser
+    browser.quit()
+
+def init_firefox_browser():
+    o = webdriver.FirefoxOptions()
+    o.headless = True
+    browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     yield browser
     browser.quit()
